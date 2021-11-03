6c55f3463c6be4fd580e36f2c3aa7437e348068c
Arthur Mårtensson <arthur0510@osloskolen.no>
2021-11-03 13:41:26 +0100

    Almost works!

---
+++ mandelbrot.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+x_res = 7016
+y_res = 4961
+x_min = -2
+x_max = 1
+y_min = (x_min - x_max)/2*(y_res / x_res)
+y_max = -y_min
+
+x_coords = np.linspace(x_min, x_max, x_res)
+y_coords = np.linspace(y_min, y_max, y_res)
+
+x_grid, y_grid = np.meshgrid(x_coords, y_coords)
+
+grid = x_grid + 1j*y_grid
+
+cgrid = np.copy(grid)
+mres = np.where(np.real(grid*np.conjugate(grid)) > 4, 1, 0)
+
+x_lims = [x for x in range(0, x_res - 1, 100)]
+y_lims = [y for y in range(0, y_res - 1, 100)]
+if x_res - 1 not in x_lims:
+    x_lims += [x_res - 1]
+if y_res - 1 not in y_lims:
+    y_lims += [y_res - 1]
+
+
+for x1, x2 in zip(x_lims[:-1], x_lims[1:]):
+    for y1, y2 in zip(y_lims[:-1], y_lims[1:]):
+
+        for i in range(2, 200):
+            #print((x1, x2))
+            #print((y1,y2))
+            grid[x1:x2,y1:y2] = grid[x1:x2,y1:y2]**2 + cgrid[x1:x2,y1:y2]
+            mres[x1:x2,y1:y2] = np.where(np.logical_and(np.real(grid[x1:x2,y1:y2]*np.conjugate(grid[x1:x2,y1:y2])) > 4,
+                                                        mres[x1:x2,y1:y2] == 0),
+                                         i,
+                                         mres[x1:x2,y1:y2])
+            grid[x1:x2, y1:y2] = np.where(mres[x1:x2, y1:y2] == 0,
+                                          grid[x1:x2, y1:y2],
+                                          0)
+            if (mres[x1:x2,y1:y2] != 0).all():
+                    break
+
+from PIL import Image as im
+image = im.fromarray(mres.astype("uint8"))
+image.save("output/image.png")
