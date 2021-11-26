2a361e33990aa9060c35ab61779021e40cb6962d
Arthur Mårtensson <arthur0510@osloskolen.no>
2021-11-26 14:16:57 +0100

    Added color

--- image.py
+++ image.py
@@ -1,7 +1,15 @@
 from PIL import Image as im
 import numpy as np
 
-mres = np.loadtxt("output/mres.txt", dtype = int)
+mres = np.loadtxt("output/mres.txt")
+mres = 512/(1+np.exp(-mres/40)) - 253
 
-image = im.fromarray(mres.astype("uint8"))
+mresr = mres.copy()/3
+mresg = mres.copy()/2
+mresb = mres
+
+imager = im.fromarray(mresr.astype("uint8"))
+imageg = im.fromarray(mresg.astype("uint8"))
+imageb = im.fromarray(mresb.astype("uint8"))
+image = im.merge("RGB", (imager, imageg, imageb))
 image.save("output/image.png")
